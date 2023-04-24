4d4012672eb84f12961a7a8261d8575eca043e28
mohibar <cibaarsom@gmail.com>
2023-04-24 07:47:41 -0700

    printf

--- print_integer.c
+++ print_integer.c
@@ -1,51 +1,28 @@
 #include "main.h"
 
 /**
- * print_decimal - prints an decimal.
+ * print_decimal - prints a decimal.
  * @ap: given arguments.
- * Return:num_argumetns_printed.
+ * Return: number of arguments printed.
  */
 
 int print_decimal(va_list ap)
 {
-	unsigned int numerator = 0;
-	unsigned int denominator = 0;
-	unsigned int number = 0;
-	unsigned int num_arguments_printed = 0;
-	int input_num = 0;
+	int input_num = va_arg(ap, int);
+	char buffer[50];
+	int num_arguments_printed;
 
-	input_num = va_arg(ap, int);
-	if (input_num < 0)
-	{
-		numerator = (input_num * -1);
-		num_arguments_printed += putchar('-');
-	}
-	else
-	{
-		numerator = input_num;
-	}
+	num_arguments_printed = snprintf(buffer, 50, "%d", input_num);
+	fputs(buffer, stdout);
 
-	denominator = numerator;
-	number = 1;
-	while (denominator > 9)
-	{
-		denominator /= 10;
-		number *= 10;
-	}
-	while (number >= 1)
-	{
-		num_arguments_printed += putchar(((numerator / number) % 10) + '0');
-		number /= 10;
-	}
 	return (num_arguments_printed);
 }
 
 /**
- * print_integer - prints a integr.
+ * print_integer - prints an integer.
  * @ap: given arguments.
- * Return:num_argumetns_printed.
+ * Return: number of arguments printed.
  */
-
 int print_integer(va_list ap)
 {
 	return (print_decimal(ap));
